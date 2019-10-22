af27b79118ae18a95b0397dbdc09cb887fc31ba4
quevicente <viceents@gmail.com>
2019-10-22 12:48:43 -0300

    alterando chamada no menu-bottom

--- src/components/menu-bottom/menu-bottom.js
+++ src/components/menu-bottom/menu-bottom.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 
-export const MenuBottom = ({newTask}) =>  {
+export const MenuBottom = ({ newTask, moreDetails }) => {
   return (
-      <View style={styles.container}>
-        <TouchableOpacity>
-          <Icon name='menu' color="#aaa" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={newTask} style={styles.newTask} >
-          <Icon name='add' color='white' size={34} style={styles.iconButton}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Icon name='more-vert' color="#aaa" />
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={moreDetails}>
+        <Icon name='menu' color="#aaa" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={newTask} style={styles.newTask} >
+        <Icon name='add' color='white' size={34} style={styles.iconButton} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon name='more-vert' color="#aaa" />
+      </TouchableOpacity>
+    </View>
   )
 }
 
@@ -28,17 +28,17 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: '#feee'
   },
-  newTask: { 
+  newTask: {
     width: 60,
     height: 60,
-    marginTop: -60, 
+    marginTop: -60,
     backgroundColor: '#5e8ded',
     borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
     elevation: 8
   },
-  iconButton: { 
+  iconButton: {
     color: '#ffff'
   }
 });
